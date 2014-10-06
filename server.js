e9660ac54fde47233c58f262f01aa613297d0bf7
woodenToaster <litmusik@yahoo.com>
2014-10-06 10:39:11 -0500

    Stopped Monday

--- server.js
+++ server.js
@@ -17,16 +17,15 @@ var app = express();
 
 //TODO: update 'what' at every location.
 
-/*
 var connection = mysql.createConnection({
-  host     : 'https://mysql.eecs.ku.edu',
+  host     : 'mysql.eecs.ku.edu',
   user     : 'chogan',
   password : '581!!'
 });
 
 connection.connect();
 connection.query('use chogan');
-*/
+
 app.use(cookieParser(credentials.cookieSecret));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -38,120 +37,29 @@ app.use(require('express-session')({
 }));
 
 app.get('/', function(req, res){
-	var agent = req.session.id;
-	var campus = [
-		{ 
-		  "id": "lied-center",
-			"where": "LiedCenter.jpg",
-			"next": {"east": "eaton-hall", "south": "dole-institute"},
-			"text": "You are outside the Lied Center."
-		},
-		{ 
-			"id": "dole-institute",
-			"where": "DoleInstituteofPolitics.jpg",
-			"next": {"east": "allen-fieldhouse", "north": "lied-center"},
-			"text": "You take in the view of the Dole Institute of Politics. This is the best part of your walk to Nichols Hall."
-		},
-		{ 
-			"id": "eaton-hall",
-			"where": "EatonHall.jpg",
-			"next": {"east": "snow-hall", "south": "allen-fieldhouse", "west": "lied-center"},
-			"text": "You are outside Eaton Hall. You should recognize here."
-		},
-		{ 
-			"id": "snow-hall",
-			"where": "SnowHall.jpg",
-			"next": {"east": "strong-hall", "south": "ambler-recreation", "west": "eaton-hall"},
-			"text": "You are outside Snow Hall. Math class? Waiting for the bus?"
-		},
-		{ 
-			"id": "strong-hall",
-			"where": "StrongHall.jpg",
-			"next": {"east": "outside-fraser", "north": "memorial-stadium", "west": "snow-hall"},
-			"what": ["coffee"],
-			"text": "You are outside Strong Hall."
-		},
-		{ 
-			"id": "ambler-recreation",
-			"where": "AmblerRecreation.jpg",
-			"next": {"west": "allen-fieldhouse", "north": "snow-hall"},
-			"text": "It's the starting of the semester, and you feel motivated to be at the Gym. Let's see about that in 3 weeks."
-		},
-		{ 
-			"id": "outside-fraser",
-		  "where": "OutsideFraserHall.jpg",
-			"next": {"west": "strong-hall","north":"spencer-museum"},
-			"what": ["basketball"],
-			"text": "On your walk to the Kansas Union, you wish you had class outside."
-		},
-		{ 
-			"id": "spencer-museum",
-			"where": "SpencerMuseum.jpg",
-			"next": {"south": "outside-fraser","west":"memorial-stadium", "east": "jail"},
-			"what": ["art"],
-			"text": "You are at the Spencer Museum of Art."
-		},
-		{ 
-			"id": "memorial-stadium",
-			"where": "MemorialStadium.jpg",
-			"next": {"south": "strong-hall","east":"spencer-museum"},
-			"what": ["ku flag"],
-			"text": "Half the crowd is wearing KU Basketball gear at the football game."
-		},
-		{ 
-			"id": "allen-fieldhouse",
-			"where": "AllenFieldhouse.jpg",
-			"next": {"north": "eaton-hall","east": "ambler-recreation","west": "dole-institute"},
-			"text": "Rock Chalk! You're at the field house."
-		},
-		{ 
-			"id": "jail",
-		  "where": "Jail.jpg",
-		  "next": {},
-		  "text": "You've been put in prison!  Maybe you were framed..."
-		}
-  ];
-  /*
-  //Add this agent to agents if it doesn't already exist
-	if(!agents.hasOwnProperty(agent)) {
-		agents[agent] = {
-		  "name": agent,
-		  "location": 'strong-hall',
-		  "inventory": ['laptop'],
-		  "campus": campus
-		};
-	}
-	next();
-	*/
-});
-
-app.get('/', function(req, res){
-	var agent = req.session.id;
-	agents[agent] = {
-	  //"sid": req.session.id;
-	  "inventory": ['laptop'],//getInventory(inv),
-	  "location": 'strong-hall',//results[0].location;
-	  "campus": campus
-  };
 	res.status(200);
 	res.sendFile(__dirname + "/index.html");
 });
 
 app.get('/:id', function(req, res){
-	var agent = req.session.id;
-	var inventory = agents[agent].inventory;
-	var campus = agents[agent].campus;
+	var agent;
+	var inventory;
+	var campus;
+	getUserInfo(agent, inventory, campus, req);
+
 	if (req.params.id == "inventory") {
 	    res.set({'Content-Type': 'application/json'});
 	    res.status(200);
 	    res.send(inventory);
 	    return;
 	}
-	if (agents[agent].location == "jail") {
+	if(agents[agent]) {
+		if (agents[agent].location == "jail") {
 			res.set({'Content-Type': 'application/json'});
 		    res.status(200);
 		    res.send(campus[campus.length - 1]);
 		    return;
+		}
 	}
 	for (var i in campus) {
 		if (req.params.id == campus[i].id) {
@@ -213,13 +121,8 @@ app.delete('/:id/:item', function (req, res) {
 	res.send("location not found");
 });
 
-app.post('/login/:name', function (req, res) {
-<<<<<<< HEAD
-	connection.query('SELECT ? FROM Users', [name], function(err, results) {
-		console.log(results);
-	});
-=======
-	
+app.put('/login/:name', function (req, res) {
+	var name = req.params.name;
 	var campus = [
 		{ 
 		  "id": "lied-center",
@@ -291,31 +194,40 @@ app.post('/login/:name', function (req, res) {
 		  "next": {},
 		  "text": "You've been put in prison!  Maybe you were framed..."
 		}
-  ];
+    ];
 
-  agents[req.session.id] = {
-				  //"sid": req.session.id;
-				  "inventory": ['laptop'],//getInventory(inv),
-				  "location": 'strong-hall',//results[0].location;
-				  "campus": campus
-			  };
-  /*
 	connection.query('SELECT ? FROM Users', [name], function(err, results) {
 		if (results) {
 			connection.query('SELECT * FROM Inventory WHERE Name = ?', 
 											 [name], function (err, inv) {
 				
 				agents[name] = {
-				  "sid": req.session.id;
-				  "inventory": getInventory(inv),
-				  "location": results[0].location;
+				  "sid": req.session.id,
+				  "inventory": [inv[0]],//getInventory(inv)
+				  "location": results[0].location,
 				  "campus": campus
 			  };
 			});
+			if(err) {
+				console.log(err);
+			}
+		} else {
+			//Add this user to the database.
+			connection.query('INSERT INTO Users (Name, Location, SID) VALUES (?, ?, ?)',
+							  [name, 'strong-hall', req.session.id], function (err) {
+			  	if(err) {
+			  		console.log(err);
+			  	}
+			});
+			connection.query('INSERT INTO Inventory (Name, Inventory) VALUES (?,?)',
+							  [name, 'laptop'], function (err) {
+				if(err) {
+					console.log(err);
+				}
+			});
 		}
 	});
-*/
->>>>>>> 9576e1cd5c6b1e97a9cef8f6cd489cbfd69885f5
+
 	res.set({'Content-Type': 'application/json'});
 	res.status(200);
 	res.send([]);
@@ -386,3 +298,22 @@ function getInventory(inventory) {
 	var placeHolder = 'test';
 
 }   
+
+function getUserInfo(agent, inventory, campus, req) {
+	agent = req.session.id;
+	connection.query('SELECT * FROM Users WHERE SID = ?', [agent], function (err, res) {
+		if(res) {
+			console.log(res);
+			agent = res.Name;
+
+		}
+	});
+	connection.query('SELECT * FROM Inventory WHERE Name = ?', [agent], function (err, inv) {
+		for (var item in inv) {
+			inventory.push(item);
+		}
+	});
+	if(agents[agent]) {
+		campus = agents[agent].campus;
+	}
+}
